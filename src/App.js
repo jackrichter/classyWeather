25832424233e667e846494a05117b0d92e01c056
jackrichter <jackrichter1@gmail.com>
2024-04-19 18:48:20 +0200

    How to handle events in Class based Components in React.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,35 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+
+    // We must bind 'this' to de event handlers we create !!
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState(curState => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState(curState => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("June 27 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
